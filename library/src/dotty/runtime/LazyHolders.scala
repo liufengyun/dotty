dbb14428750c0c13a7bf2ed0458eee3a31e3bcc0
Liu Fengyun <liu@fengy.me>
2018-05-02 06:02:52 +0200

    Add tests and implementation

--- library/src/dotty/runtime/LazyHolders.scala
+++ library/src/dotty/runtime/LazyHolders.scala
@@ -4,41 +4,41 @@ package dotty.runtime
  * Classes used as holders for local lazy vals
  */
 class LazyInt {
-  var value: Int = _
+  var value: Int = 0
   @volatile var initialized: Boolean = false
 }
 
 class LazyLong {
-  var value: Long = _
+  var value: Long = 0
   @volatile var initialized: Boolean = false
 }
 
 class LazyBoolean {
-  var value: Boolean = _
+  var value: Boolean = false
   @volatile var initialized: Boolean = false
 }
 
 class LazyDouble {
-  var value: Double = _
+  var value: Double = 0
   @volatile var initialized: Boolean = false
 }
 
 class LazyByte {
-  var value: Byte = _
+  var value: Byte = 0
   @volatile var initialized: Boolean = false
 }
 
 class LazyRef {
-  var value: AnyRef = _
+  var value: AnyRef = null
   @volatile var initialized: Boolean = false
 }
 
 class LazyShort {
-  var value: Short = _
+  var value: Short = 0
   @volatile var initialized: Boolean = false
 }
 
 class LazyChar {
-  var value: Char = _
+  var value: Char = 0
   @volatile var initialized: Boolean = false
 }
